303483a61f0013b38d1071b5bf4fee3da34b04c9
Anubhav Kumar <44000628+anubhavkmr8@users.noreply.github.com>
2020-10-27 23:35:08 +0530

    Entropy_Textbook_Sentence and Textbook_Sentence

---
+++ Entropy/Entropy_Textbook_Sentence.py
@@ -0,0 +1,39 @@
+import re
+import sys
+import random
+from math import log
+
+# Function to compute the base-2 logarithm of a floating point number.
+def log2(number):
+    return log(number) / log(2)
+
+
+# Function to normalise the text.
+cleaner = re.compile('[^a-z]+')
+
+
+def clean(text):
+    return cleaner.sub(' ', text)
+
+
+# Dictionary for letter counts
+letter_frequency = {}
+
+# Read and normalise input text
+text = input()
+
+# Count letter frequencies
+for letter in text:
+    if letter in letter_frequency:
+        letter_frequency[letter] += 1
+    else:
+        letter_frequency[letter] = 1
+
+# Calculate entropy
+length_sum = 0.0
+for letter in letter_frequency:
+    probability = float(letter_frequency[letter]) / len(text)
+    length_sum += probability * log2(probability)
+
+# Output
+sys.stdout.write('Entropy: %f bits per character\n' % (-length_sum))
